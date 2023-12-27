93d56db12363dfb5dfba0bde71b6ba029d9e3dd0
David Nava <david.nava.garcia4@gmail.com>
2023-12-26 20:59:14 -0600

    Adición de las funciones de editar y crear para la vista de categorias

--- SistemaWeb_UnidadPracticas/Controllers/SIGUPController.cs
+++ SistemaWeb_UnidadPracticas/Controllers/SIGUPController.cs
@@ -42,6 +42,39 @@ namespace SistemaWeb_UnidadPracticas.Controllers
             return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
             /*El json da los datos, jala los datos de esa lista, en data*/
         }
+        [HttpPost]
+        public JsonResult GuardarCategoria(EN_CategoriaHerramienta objeto) /*De este json se puede controlar que mas ver, igualar elementos, etc*/
+        {
+            object resultado;/*Va a permitir almacenar cualquier tipo de resultado (en este caso int o booelan, dependiendi si es creacion o edicion)*/
+            string mensaje = string.Empty;
+
+            if (objeto.idCategoria == 0)/*Es decir, si el id es 0 en inicio (el valor es 0 inicialmente) significa que es
+             una categoria nueva, por lo que se ha dado dando clic con el boton de crear*/
+            {
+                resultado = new RN_CategoriaHerramienta().Registrar(objeto, out mensaje);/*El metodo registrar
+                 de tipo int, devuelve el id registrado*/
+            }
+            else
+            {/*Pero si el id es diferente de 0, es decir ya existe, entonces se esta editando
+                 a una categoria, por lo que indica que se ha dado clic en el boton de editar, eso lo comprobamos
+                 con los alert comentados*/
+                resultado = new RN_CategoriaHerramienta().Editar(objeto, out mensaje);
+            }
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        [HttpPost]
+        public JsonResult EliminarCategoria(string id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new RN_CategoriaHerramienta().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
 
         #endregion
 
