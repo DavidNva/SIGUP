9fb82d7a2688b376f6c7f342fa5ab4c2f8b1eddf
David Nava <david.nava.garcia4@gmail.com>
2023-12-31 15:37:21 -0600

    Se añadió los procedimientos almacenados de herramientas y reacomodo del diseño del modal de herramientas

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -81,7 +81,7 @@ GO
 
 create procedure sp_RegistrarMarca(--Hay un indice unico para el nombre completo del Categoria 
     --@IDCategoria int,---El id es Identity
-    @Descripcion varchar(100),--Tiene indice compuesto con Apellidos
+    @DescripcionMarca varchar(100),--Tiene indice compuesto con Apellidos
     @Activo bit,
     @Mensaje varchar(500) output,
     @Resultado int output
@@ -91,15 +91,15 @@ create procedure sp_RegistrarMarca(--Hay un indice unico para el nombre completo
 as
 begin
     SET @Resultado = 0 --No permite repetir un mismo correo, ni al insertar ni al actualizar
-    IF NOT EXISTS (SELECT * FROM marca_herramienta WHERE Descripcion = @Descripcion)
+    IF NOT EXISTS (SELECT * FROM marca_herramienta WHERE Descripcion = @DescripcionMarca)
     begin 
         insert into marca_herramienta(Descripcion, Activo) values 
-        (@Descripcion, @Activo)
+        (@DescripcionMarca, @Activo)
         --La función SCOPE_IDENTITY() devuelve el último ID generado para cualquier tabla de la sesión activa y en el ámbito actual.
         SET @Resultado = scope_identity()
     end 
     else 
-     SET @Mensaje = 'La categoria ya existe'
+     SET @Mensaje = 'La marca ya existe'
 end
 
 
@@ -127,3 +127,116 @@ exec sp_RegistrarCategoria 'LIBROS DONADOS',1,'',1
 exec sp_RegistrarCategoria 'LIBROS INFANTILES',1,'',1
 
 select * from Categoria_Herramienta
+
+
+go
+use UDP_CONTROL
+
+GO
+create procedure sp_RegistrarHerramienta(--Nuevo sp que registra igual el Herramienta con su ejemplar  la vez
+    @IdHerramienta int,--Es asignado por administrador al insertar
+    @Nombre nvarchar(60),
+    @Cantidad int,
+    --Llaves foraneas
+    @IDMarca int,
+    @IDCategoria int,--Tiene un DEFAULT en Sala = S0001 (Sala General)
+    @Observaciones varchar(500), --Definido como default: EN BUEN ESTADO
+    @Activo bit,--Mejor al registrarlo darlo por default como 1, no tiene sentido regitrar y darlo como inactivo
+    @Mensaje varchar(500) output,
+    @Resultado int output
+    )
+as
+begin
+    begin try 
+        declare @idCodigoHerramienta int = 0
+        SET @Resultado = 0 --No permite repetir un mismo correo, ni al insertar ni al actualizar
+        IF NOT EXISTS (SELECT * FROM Herramienta WHERE IdHerramienta = @IdHerramienta)
+        begin
+            begin transaction registroHerramienta
+            insert into Herramienta(IdHerramienta, Nombre, Cantidad,  id_marca, id_categoria,Observaciones, Activo) values 
+            (@IdHerramienta, @Nombre, @Cantidad, @IDMarca, @IDCategoria,  @Observaciones, 1)
+            --La función SCOPE_IDENTITY() devuelve el último ID generado para cualquier tabla de la sesión activa y en el ámbito actual.
+            SET @Resultado = scope_identity() 
+            set @idCodigoHerramienta = SCOPE_IDENTITY()--obtiene el ultimo id que se esta registrando
+            
+            --insert into Ejemplar(ID_Herramienta, Activo)
+            --values(@@idCodigoHerramienta,1)
+            commit transaction registroHerramienta
+        end
+        else 
+        SET @Mensaje = 'El código de la Herramienta ya existe*'
+    end try
+    begin catch
+        set @Resultado = 0
+        set @Mensaje = ERROR_MESSAGE()
+        rollback transaction registroHerramienta 
+    end catch
+    
+end 
+go
+create procedure sp_EditarHerramienta(
+    @IdHerramienta int,--Es asignado por administrador al insertar
+    @Nombre nvarchar(60),
+    @Cantidad int,
+    --Llaves foraneas
+    @IDMarca int,
+    @IDCategoria int,--Tiene un DEFAULT en Sala = S0001 (Sala General)
+    @Observaciones varchar(500), --Definido como default: EN BUEN ESTADO
+    @Activo bit,--Mejor al registrarlo darlo por default como 1, no tiene sentido regitrar y darlo como inactivo
+    @Mensaje varchar(500) output,
+    @Resultado int output
+    )
+as
+begin
+    SET @Resultado = 0 --No permite repetir un mismo correo, ni al insertar ni al actualizar
+    IF NOT EXISTS (SELECT * FROM Herramienta WHERE nombre = @Nombre and IdHerramienta != @IdHerramienta)
+    begin 
+        update Herramienta set
+        Nombre = @Nombre,
+        Cantidad = @Cantidad,
+        id_marca = @IDMarca, 
+        id_categoria = @IDCategoria,  
+        Observaciones = @Observaciones, 
+        Activo = @Activo 
+        where IdHerramienta = @IdHerramienta
+        --La función SCOPE_IDENTITY() devuelve el último ID generado para cualquier tabla de la sesión activa y en el ámbito actual.
+        SET @Resultado = 1 --true
+    end 
+    else 
+     SET @Mensaje = 'El código de la Herramienta ya existe'
+end
+
+go
+--PARA QUE ESTO FUNCIONE BIEN, ENTONCES EL DETALLEPRESTAMO EN SU COLUMNA ACTIVO
+    --CUANDO EL ADMIN ACTUALICE UN PRESTAMO DE ACTIVO A INACTIVO (ES DECIR QUE VA A DEVOLVER EL Herramienta Y YA NO VA ESTAR EN PRESTAMO)
+    --EL ACTIVO DE PRESTAMO SE ACTUALIZA A 0 Y ENTONCES AUTOMAICAMENTE TAMBIEN EL ACTIVO DE DETALLE PRESTAMO DEBE SER 0
+    --CON ESO VALIDAREMOS ESTA SELECCION PARA ELIMINAR UN Herramienta NO DEBE HABER UN Herramienta RELACIONADO CON EJEMPLAR CUYO EJEMPLAR ESTE RELACIONADO 
+    --A UN DETALLEPRESTAMO CUYO A SU VEZ ESTA RELACIONADO CON PRESTAMO Y ESTE ACTIVO DICHO PRESTAMO. ENTONCES PARA PODER ELMINAR
+    --NO DEBE ESTAR UN ID CON UN EJEMPLAR EN DETALLE PRESTAMO QUE AUN ESTE ACTIVO.
+go
+create procedure sp_EliminarHerramienta(
+    @IdHerramienta int,
+    @Mensaje varchar(500) output,
+    @Resultado int output
+    )
+as
+begin
+    SET @Resultado = 0 --No permite repetir un mismo correo, ni al insertar ni al actualizar
+    IF NOT EXISTS (
+	select * from herramienta h
+    inner join Detalle_Prestamo dp on dp.IDHerramienta = h.IdHerramienta
+    inner join Prestamo p on p.IdPrestamo = dp.IdPrestamo and p.Activo = 1
+    where h.IdHerramienta = @IdHerramienta)--No podemos eliminar un Herramienta si ya esta incluido en una venta
+    begin 
+        delete top(1) from Herramienta where IdHerramienta = @IdHerramienta
+
+        -- delete top(1) from Prestamo where IdPrestamo = @IdPrestamo
+        --Como el ejemplar tiene una relacion con idHerramienta y un deletecascade se eliminará automaticamente al eliminar el Herramienta
+        --La función SCOPE_IDENTITY() devuelve el último ID generado para cualquier tabla de la sesión activa y en el ámbito actual.
+        SET @Resultado = 1 --true
+    end 
+    else 
+     SET @Mensaje = 'La Herramienta se encuentra relacionada a un préstamo'
+end 
+go
+select * from herramienta
